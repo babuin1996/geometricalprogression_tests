8d8779442697b1c75d2ec306b6b96dd5c62b5967
krasnyzmey <vstrukoff@yandex.ru>
2019-02-14 15:05:45 +0500

    new tests

--- classes/GeometricalProgression.php
+++ classes/GeometricalProgression.php
@@ -17,25 +17,34 @@ class GeometricalProgression
 
     public function check($output = null)
     {
+        foreach ($this->array as $a) {
+            if (!is_int($a) || $a == 0) {
+                if ($output === true) {
+                    return "This is NOT a geometrical progression";
+                } else {
+                    return false;
+                }
+            }
+        }
+
         if (sizeof($this->array) <= 1)
             return True;
 
-        $this->ratio = $this->array[1]/$this->array[0];
+        $this->ratio = $this->array[1] / $this->array[0];
 
-
-        for($i=1; $i<sizeof($this->array); $i++)
-        {
-            if (($this->array[$i]/($this->array[$i-1])) != $this->ratio)
-            {
+        for ($i = 1; $i < sizeof($this->array); $i++) {
+            if (($this->array[$i] / ($this->array[$i - 1])) != $this->ratio) {
                 $this->isGeometricalProgression = false;
             } else {
                 $this->isGeometricalProgression = true;
             }
         }
 
-
-        if($output === true){
-            return ("This is a geometrical progression with ratio = $this->ratio");
+        if ($output === true) {
+            if ($this->isGeometricalProgression === true)
+                return ("This is a geometrical progression with ratio = $this->ratio");
+        } else {
+            return "This is NOT a geometrical progression";
         }
     }
 }
--- tests/GeometricalProgressionTest.php
+++ tests/GeometricalProgressionTest.php
@@ -33,4 +33,52 @@ class GeometricalProgressionTest extends TestCase
         $obj->check();
         $this->assertEquals($obj->ratio, 3);
     }
+
+    public function testCheckNegativeValue(){
+        $progression = [-2,4,-8,16,-32];
+        $obj = new GeometricalProgression($progression);
+        $obj->check();
+        $this->assertTrue($obj->isGeometricalProgression);
+    }
+
+    public function testCheckZeroValueInteger(){
+        $progression = [2,0,4,0,8];
+        $obj = new GeometricalProgression($progression);
+        $obj->check();
+        $this->assertTrue(!$obj->isGeometricalProgression);
+    }
+
+    public function testCheckZeroValueFloat(){
+        $progression = [2,0.0,4,8];
+        $obj = new GeometricalProgression($progression);
+        $obj->check();
+        $this->assertTrue(!$obj->isGeometricalProgression);
+    }
+
+    public function testCheckZeroValueString(){
+        $progression = [2,'0',4,8];
+        $obj = new GeometricalProgression($progression);
+        $obj->check();
+        $this->assertTrue(!$obj->isGeometricalProgression);
+    }
+
+    public function testArrayWithString(){
+        $progression = [2,'string',4,8];
+        $obj = new GeometricalProgression($progression);
+        $obj->check();
+        $this->assertTrue(!$obj->isGeometricalProgression);
+    }
+
+    public function testArrayWithBoolean(){
+        $progression = [2,true,4,8];;
+        $obj = new GeometricalProgression($progression);
+        $obj->check();
+        $this->assertTrue(!$obj->isGeometricalProgression);
+    }
+    public function testArrayWithArray(){
+        $progression = [2,[1,2,4],4,8];;
+        $obj = new GeometricalProgression($progression);
+        $obj->check();
+        $this->assertTrue(!$obj->isGeometricalProgression);
+    }
 }
